403498f454bd72fdcf1c48d0d316ee832ddc73b4
TungLe <thanhtung12121212@gmail.com>
2022-10-08 22:27:27 +0700

    Dựng khung Layout mặc định

---
+++ src/components/Layouts/Components/Header/index.js
@@ -0,0 +1,14 @@
+import className from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx =  className.bind(styles)
+
+function Header() {
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}></div>
+        </header>
+    )
+}
+
+export default Header;
